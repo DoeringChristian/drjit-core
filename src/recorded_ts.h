bf67ca6f9562f4f5bc673eb7c292e0900d4b2e70
Christian Döring <christian.doering@tum.de>
2024-04-03 21:49:48 +0200

    Implemented passthrough for RecordedThreadState

--- src/recorded_ts.h
+++ src/recorded_ts.h
@@ -15,10 +15,15 @@ struct Operation{
 using VariableCache = tsl::robin_map<uint32_t, uint32_t>;
 
 struct RecordedThreadState: ThreadState{
+    
+    RecordedThreadState(ThreadState *internal){
+        this->internal = internal;
+    }
 
     Task *launch(Kernel kernel, uint32_t size, 
                  std::vector<ScheduledVariable> &scheduled) override{
 
+        // Record the operation
         uint32_t start = this->dependencies.size();
         for ( ScheduledVariable &sv : scheduled ){
             this->dependencies.push_back(this->get_or_insert_variable(sv.index));
@@ -28,56 +33,84 @@ struct RecordedThreadState: ThreadState{
             .type = OpType::KernelLaunch,
             .dependency_range = std::pair(start,end),
         });
+
+        return this->internal->launch(kernel, size, scheduled);
     }
 
     /// Fill a device memory region with constants of a given type
     void memset_async(void *ptr, uint32_t size, uint32_t isize,
-                      const void *src) override;
+                      const void *src) override{
+        return this->internal->memset_async(ptr, size, isize, src);
+    }
 
     /// Reduce the given array to a single value
     void reduce(VarType type, ReduceOp rtype, const void *ptr, uint32_t size,
-                void *out) override;
+                void *out) override{
+        return this->internal->reduce(type, rtype, ptr, size, out);
+    }
 
     /// 'All' reduction for boolean arrays
-    bool all(uint8_t *values, uint32_t size) override;
+    bool all(uint8_t *values, uint32_t size) override{
+        return this->internal->all(values, size);
+    }
 
     /// 'Any' reduction for boolean arrays
-    bool any(uint8_t *values, uint32_t size) override;
+    bool any(uint8_t *values, uint32_t size) override{
+        return this->internal->any(values, size);
+    }
 
     /// Exclusive prefix sum
     void prefix_sum(VarType vt, bool exclusive, const void *in, uint32_t size,
-                    void *out) override;
+                    void *out) override{
+        return this->internal->prefix_sum(vt, exclusive, in, size, out);
+    }
 
     /// Mask compression
-    uint32_t compress(const uint8_t *in, uint32_t size, uint32_t *out) override;
+    uint32_t compress(const uint8_t *in, uint32_t size, uint32_t *out) override{
+        return this->internal->compress(in, size, out);
+    }
 
     /// Compute a permutation to reorder an integer array into discrete groups
     uint32_t mkperm(const uint32_t *values, uint32_t size,
                     uint32_t bucket_count, uint32_t *perm,
-                    uint32_t *offsets) override;
+                    uint32_t *offsets) override{
+        return this->internal->mkperm(values, size, bucket_count, perm, offsets);
+    }
 
     /// Perform a synchronous copy operation
-    void memcpy(void *dst, const void *src, size_t size) override;
+    void memcpy(void *dst, const void *src, size_t size) override{
+        return this->internal->memcpy(dst, src, size);
+    }
 
     /// Perform an assynchronous copy operation
-    void memcpy_async(void *dst, const void *src, size_t size) override;
+    void memcpy_async(void *dst, const void *src, size_t size) override{
+        return this->internal->memcpy_async(dst, src, size);
+    }
 
     /// Sum over elements within blocks
     void block_sum(enum VarType type, const void *in, void *out, uint32_t size,
-                   uint32_t block_size) override;
+                   uint32_t block_size) override{
+        return this->internal->block_sum(type, in, out, size, block_size);
+    }
 
     /// Asynchronously update a single element in memory
-    void poke(void *dst, const void *src, uint32_t size) override;
+    void poke(void *dst, const void *src, uint32_t size) override{
+        return this->internal->poke(dst, src, size);
+    }
 
-    void aggregate(void *dst, AggregationEntry *agg, uint32_t size) override;
+    void aggregate(void *dst, AggregationEntry *agg, uint32_t size) override{
+        return this->internal->aggregate(dst, agg, size);
+    }
 
     // Enqueue a function to be run on the host once backend computation is done
-    void enqueue_host_func(void (*callback)(void *), void *payload) override;
+    void enqueue_host_func(void (*callback)(void *), void *payload) override{
+        return this->internal->enqueue_host_func(callback, payload);
+    }
 
     /// LLVM: reduce a variable that was previously expanded due to dr.ReduceOp.Expand
-    void reduce_expanded(VarType, ReduceOp, void *, uint32_t,
-                         uint32_t) override {
-      jitc_raise("jitc_reduce_expanded(): unsupported by CUDAThreadState!");
+    void reduce_expanded(VarType vt, ReduceOp op, void *data, uint32_t exp,
+                         uint32_t size) override {
+        return this->internal->reduce_expanded(vt, op, data, exp, size);
     }
 
 private:
