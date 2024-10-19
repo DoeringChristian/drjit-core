b14ad48926cb58f428f5a5764a8368b37e3f0bd2
Christian Döring <christian.doering@tum.de>
2024-10-19 12:49:15 +0200

    Added registry_fill_ptrs function

--- src/registry.h
+++ src/registry.h
@@ -22,7 +22,11 @@ extern void jitc_registry_remove(const void *ptr);
 extern uint32_t jitc_registry_id(const void *ptr);
 
 /// Return the largest instance ID for the given domain
+/// If the \c domain is a nullptr, it returns the id_bound over all registries
 extern uint32_t jitc_registry_id_bound(JitBackend backend, const char *domain);
+
+/// Fills the \c dest pointer array with all pointers registered in the registry
+void extern jitc_registry_fill_ptrs(JitBackend backend, void **dest);
 
 /// Return the pointer value associated with a given instance ID
 extern void *jitc_registry_ptr(JitBackend backend, const char *domain, uint32_t id);
